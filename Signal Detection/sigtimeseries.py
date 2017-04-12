540838c945c37592c311de0d619e8e116029afc6
Mikhail Lara <tahuichimiguel@gmail.com>
2017-04-11 20:50:23 -0400

    sigtimeseries

---
+++ Signal Detection/sigtimeseries.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+# Detect Locations of Sudden Changes in Signal
+def delta_slope(series)
+    
+def delta_outlier(series, window=50, advance = 400, Z=2.5):
+    
+
+# Calculate Rate of Sudden Change
+def delta_rate(series)
+    
+# Detect Sudden Change is Signal Noise
+def delta_noise(series)
